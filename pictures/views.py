f03372635c4a781ca5c3eb0c0c4098ceb6c7b997
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-07-03 13:57:25 +0300

    add views

--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+#import images
+from models import Images
+from django.shortcuts import render
+from django.http  import HttpResponse,Http404
+from decouple import config,Csv
+
+
+# Create your views here.
+def my_gallery(request):
+    images = Images.my_gallery()
+    return render(request, 'pics/home.html', {"images":images})
+
+def show_images(request):
+   
+    try:
+        images = Images.objects.all()
+        ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+
+    except Images.DoesNotExist:
+        raise Http404()
+    return render(request,"pics/my_gallery.html", {"images":images, "ALLOWED_HOSTS":ALLOWED_HOSTS})
+   
+   
+def search_results(request):
+
+    if 'images' in request.GET and request.GET["images"]:
+        search_term = request.GET.get("images")
+        searched_images = Images.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'pics/search.html',{"message":message,"gallery": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'pics/search.html',{"message":message})
+
+
+
+
 
 # Create your views here.
